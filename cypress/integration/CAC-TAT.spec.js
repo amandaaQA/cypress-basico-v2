365ffafdf444dbe0e6e06c5d6bff5c9ef745b4eb
Amanda Freitas <amandkelvi@gmail.com>
2024-10-23 18:44:07 -0300

    update exercices section 3

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -84,7 +84,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
     })
 
-    it.only('Valida se exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+    it('Valida se exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
         cy.get('#firstName').type(firstName)
         cy.get('#lastName').type(lastName)
         cy.get('#email').type(email)
